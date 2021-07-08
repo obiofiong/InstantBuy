f2cb0284a0c1ba2781f246a72cce14ccc0065463
John <johnobiofiong1@gmail.com>
2021-07-08 13:41:49 +0100

    initial commit

--- src/Components/Main/Feed/Products/Product.js
+++ src/Components/Main/Feed/Products/Product.js
@@ -1,7 +1,25 @@
+import item from "../../../../Utils/FakeStore/1.jpg";
+import { ShoppingBagIcon } from "@heroicons/react/solid";
+
 function Product() {
 	return (
-		<div className="w-32 h-52 rounded-xl bg-blue-300 mx-2 hover:shadow-lg filter drop-shadow-lg">
-			<div className=""></div>
+		<div className="w-36 h-60 px-3 py-2 rounded-xl border bg-white mx-2 hover:shadow-md group ">
+			<div className=" cursor-pointer py-1">
+				<img
+					className="h-40"
+					// src={require("../../../../Utils/FakeStore/1.jpg")}
+					src={item}
+				/>
+			</div>
+			<div className="text-xs">
+				<p className=" ">Expensive HIgh Cool Quality Bag</p>
+				<div className="flex items-center justify-between">
+					<p className=" font-bold">$230,000</p>
+					<span className="cursor-pointer p-1 rounded-full border sm:opacity-0 group-hover:opacity-100 transition duration-200 ease-in-out hover:bg-gray-100">
+						<ShoppingBagIcon className="h-5" />
+					</span>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/Components/Main/Feed/Products/Products.js
+++ src/Components/Main/Feed/Products/Products.js
@@ -1,10 +1,17 @@
 import Product from "./Product";
-import productsList from "../../../../FakeStore/Products";
+import productsList from "../../../../Utils/FakeStore/products.js";
 
 function Products() {
 	console.log(productsList);
 	return (
-		<div className="py-3 my-3">
+		<div className="py-3 my-3 flex w-64 scrollbar-hidey overscroll-x-scroll ">
+			{/* {productList.slice(1, 5).map((product) => (
+				<Product key={product.id} product={product} />
+			))} */}
+			<Product />
+			<Product />
+			<Product />
+			<Product />
 			<Product />
 		</div>
 	);
