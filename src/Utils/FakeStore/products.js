f63c8240fd1fb7ceb413dbc43d0d14fe520bffaf
John Obiofiong <45612996+obiofiong@users.noreply.github.com>
2021-09-19 13:18:01 +0100

    Update products.js

--- src/Utils/FakeStore/products.js
+++ src/Utils/FakeStore/products.js
@@ -168,7 +168,7 @@ const products = [
 	},
 	{
 		id: 19,
-		title: "Opna Women's Short Sleeve Moisture",
+		title: "Opna Women's Short Sleeve Moisture cloth",
 		price: 7.95,
 		description:
 			"100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
