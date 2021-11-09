a26f2f65048aa1437f0a2b9e373e039ca3edc342
obiofiong <johnobiofiong1@gmail.com>
2021-11-09 07:19:27 +0100

    removed unused variable

--- src/Components/Main/Sidebar/SidebarItem.js
+++ src/Components/Main/Sidebar/SidebarItem.js
@@ -2,8 +2,13 @@ function SidebarItem({ name, img }) {
 	return (
 		<div
 			className="flex flex-col items-center text-xs text-center p-3 my-3 rounded-lg hover:bg-gray-100 hover:shadow-md transition duration-200 ease-in-out cursor-pointer
-						sm:flex-row sm:my-0 sm:p-1 sm:rounded-2xl">
-			<img src={img} className="sm:h-6 sm:mx-1 md:mx-3" />
+						sm:flex-row sm:my-0 sm:p-1 sm:rounded-2xl"
+		>
+			<img
+				src={img}
+				className="sm:h-6 sm:mx-1 md:mx-3"
+				alt={`${name}-pic`}
+			/>
 			<p className="">{name}</p>
 		</div>
 	);
